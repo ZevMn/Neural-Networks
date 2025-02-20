267077121033631e808f06f6c2e32117e3b817c0
ShresthShri <79257227+ShresthShri@users.noreply.github.com>
2025-02-20 16:59:50 +0000

    small change

--- part2_house_value_regression.py
+++ part2_house_value_regression.py
@@ -4,6 +4,7 @@ import pickle
 import numpy as np
 import pandas as pd
 from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
 from itertools import product
 
 
